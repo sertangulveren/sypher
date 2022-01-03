dbcbe45136222eb37c31d200cb8237738dcd2d8a
Sertan Gülveren <sertangulveren@gmail.com>
2022-01-03 23:23:34 +0300

    basic error printer created

---
+++ internal/utils/printer.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+)
+
+func ExitWithMessage(err error, msg string)  {
+	if err == nil {
+		return
+	}
+	if os.Getenv("DEBUG") != "" {
+		debug.PrintStack()
+	}
+	fmt.Println(err)
+
+	fmt.Println(msg)
+
+	os.Exit(1)
+}
+func PanicWithError(err error)  {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/utils/printer_test.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestShouldPanic(t *testing.T) {
+	defer func() { recover() }()
+
+	PanicWithError(errors.New("calm down"))
+
+	t.Errorf("should have panicked")
+}
+
+func TestShouldNotPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("should not panic!")
+		}
+	}()
+
+	PanicWithError(nil)
+}
+
+func TestShouldExitWithMessage(t *testing.T) {
+	if os.Getenv("EXIT_TEST") == "true" {
+		ExitWithMessage(errors.New("nice error"),"im running on subprocess")
+		return
+	}
+
+	c := exec.Command(os.Args[0], "-test.run=TestShouldExitWithMessage")
+	c.Env = append(os.Environ(), "EXIT_TEST=true")
+	err := c.Run()
+
+	_, ok := err.(*exec.ExitError)
+	if !ok {
+		t.Errorf("OS Exit not performed")
+	}
+}
+
+func TestShouldNotExit(t *testing.T) {
+	ExitWithMessage(nil,"")
+}
