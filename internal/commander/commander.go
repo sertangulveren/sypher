4eb44d5f74433bf511e5ae250a2a40d6450751a5
Sertan Gülveren <sertangulveren@gmail.com>
2022-01-03 23:29:32 +0300

    generate credentials by arguments

---
+++ internal/commander/commander.go
@@ -0,0 +1,40 @@
+package commander
+
+import (
+	"fmt"
+	"github.com/sertangulveren/sypher"
+	"github.com/sertangulveren/sypher/internal/shared"
+	"github.com/sertangulveren/sypher/internal/utils"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+// Generate new credential(s)
+func Generate() {
+	if _, err := os.Stat(shared.ContentDir); os.IsNotExist(err) {
+		err := os.Mkdir(shared.ContentDir, os.ModePerm)
+		utils.ExitWithMessage(err, shared.CannotCreateWorkingDirectory)
+	}
+	pieces := shared.CmdArgs()
+	if len(pieces) == 0 {
+		pieces = shared.DefaultConfigArgs
+	}
+
+	// create credential for each pieces
+	for _, item := range pieces {
+		s := sypher.Sypher{
+			Name: item,
+			Key:  utils.GenerateKey(),
+		}
+		if _, err := os.Stat(s.FileName()); err == nil {
+			fmt.Println("Ignored: ", s.Name)
+			continue
+		}
+		s.Write([]byte(shared.DefaultContent))
+		s.WriteKey()
+		fmt.Println("Created: ", s.Name)
+	}
+	fmt.Println(shared.Done)
+}
