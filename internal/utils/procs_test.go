fe9c3a652342eb3489c0bd4b7dad16ca57f325c5
Sertan Gülveren <sertangulveren@gmail.com>
2022-01-03 23:25:02 +0300

    cryption tests

---
+++ internal/utils/procs_test.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"bytes"
+	"testing"
+)
+
+var (
+	contentBase64 = []byte("SGVsbG8gV29ybGQ")
+	contentPlain = []byte("Hello World")
+	testData = []byte("0")
+	testEncryptedData = "bac57c92c77132f295e7485ce0b6dc3e04e8e7b7b3a19fb7063ed21a72"
+)
+
+const TestAesKey = "12345678901234567890123456789012"
+
+func TestEncodeBase64(t *testing.T) {
+	result := EncodeBase64(contentPlain)
+	if !bytes.Equal(contentBase64, result) {
+		t.Errorf("expected %s, got %s", contentBase64, result)
+	}
+}
+
+func TestDecodeBase64(t *testing.T) {
+	result := DecodeBase64(contentBase64)
+	if !bytes.Equal(contentPlain, result) {
+		t.Errorf("expected %s, got %s", contentPlain, result)
+	}
+}
+
+func TestEncrypt(t *testing.T) {
+	// no error expected
+	_ = Encrypt(TestAesKey, testData)
+}
+
+func TestDecrypt(t *testing.T) {
+	encrypted := Encrypt(TestAesKey, testData)
+	res := Decrypt(TestAesKey, encrypted)
+
+	if !bytes.Equal(testData, res) {
+		t.Errorf("expected %x, got %x", testData, res)
+	}
+}
